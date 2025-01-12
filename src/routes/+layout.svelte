93798b2cba0169b41657b5c666774bfbdeeead11
mik135 <michaelsavior1@gmail.com>
2025-01-12 18:52:12 +0100

    Home page v1

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,12 @@
 	let { children } = $props();
 </script>
 
+<svelte:head>
+	<link href="https://cdn.jsdelivr.net/npm/daisyui@5.0.0-beta.1/daisyui.css" rel="stylesheet" type="text/css" />
+</svelte:head>
+
+<div class="navbar bg-black shadow-sm">
+	<a class="btn btn-ghost text-2xl font-mono text-white" href="/">fork..</a>
+</div>
+
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,41 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+
+</script>
+
+<section>
+    <div class="hero bg-base-200 min-h-screen">
+        <div class="hero-content text-center flex flex-col">
+          <div class="max-w-xl">
+            <h1 class="text-5xl font-bold">Create <span class="font-extrabold bg-gradient-to-r from-blue-600 via-purple-500 to-pink-500 text-transparent bg-clip-text">the perfect FAQ page</span> in minutes</h1>
+            <p class="py-6 text-lg">
+                Turn your scattered whatsapp messages into sleek, organized FAQ pages. Finally, a tool for people who hate having to repeat themselves everytime.
+            </p>
+            <form method="POST">
+                <div class="md:flex md:gap-3 md:mx-auto">
+                    <label class="input validator w-full h-12">
+                        <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" fill="none" stroke="currentColor"><rect width="20" height="16" x="2" y="4" rx="2"></rect><path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path></g></svg>
+                        <input type="email" placeholder="Enter your email" name="email" required/>
+                      </label>
+                      <div class="validator-hint hidden">Enter valid email address</div>
+                
+                    <button class="rounded-md bg-gradient-to-r from-blue-600 to-purple-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:opacity-90 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 w-full mt-2 md:mt-0 md:w-32">Get Started</button>
+                </div>
+                <div class="mt-0 text-sm text-gray-600">
+                    No credit card required • Set up in 5 minutes
+                </div>
+            </form>
+          </div>
+          <div class="mt-5 md:mt-10 mb-0 flex items-center justify-center gap-x-6">
+            <button class="text-sm font-semibold text-gray-900">
+              View Examples <span aria-hidden="true">→</span>
+            </button>
+          </div>
+          
+        </div>
+      </div>
+</section>
+
+
+
+<style>
+</style>
